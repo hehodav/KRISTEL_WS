d947e6bc7859da855105b85d683a31c2eb0d6114
hehodav <hehodav@gmail.com>
2024-02-22 14:37:02 +0100

    All changes 20240222 am

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,7 @@
+/*************************************************************************
+*                            NAVBAR                                      *
+**************************************************************************/
+
 
 // Sélectionne l'élément de la barre de navigation
 const navbar = document.querySelector(".navbar"); 
@@ -22,45 +26,60 @@ window.addEventListener("scroll", function() {
         navbar.style.backgroundColor = ""; } }); 
 
 
+/*************************************************************************
+*                            HOME PAGE                                   *
+**************************************************************************/
 
+/**************************       HEADER     *****************************/
 
 // Survol des liens vers les réseaux sociaux
 const logoFacebook = document.querySelector("#logoFacebook");
 logoFacebook.addEventListener("mouseover", function() {
-    logoFacebook.src = "./assets/logos/facebook_black.png";
+    logoFacebook.src = "./assets/logos/facebook_black.webp";
 })
 logoFacebook.addEventListener("mouseout", function() {
-    logoFacebook.src = "./assets/logos/facebook_red.png";
+    logoFacebook.src = "./assets/logos/facebook_red.webp";
 })
 
 const logoInstagram = document.querySelector("#logoInstagram");
 logoInstagram.addEventListener("mouseover", function() {
-    logoInstagram.src = "./assets/logos/Instagram_black.png";
+    logoInstagram.src = "./assets/logos/Instagram_black.webp";
 })
 logoInstagram.addEventListener("mouseout", function() {
-    logoInstagram.src = "./assets/logos/Instagram_red.png";
+    logoInstagram.src = "./assets/logos/Instagram_red.webp";
 })
 
 const logoTikTok = document.querySelector("#logoTikTok");
 logoTikTok.addEventListener("mouseover", function() {
-    logoTikTok.src = "./assets/logos/TikTok_black.png";
+    logoTikTok.src = "./assets/logos/TikTok_black.webp";
 })
 logoTikTok.addEventListener("mouseout", function() {
-    logoTikTok.src = "./assets/logos/TikTok_red.png";
+    logoTikTok.src = "./assets/logos/TikTok_red.webp";
 })
 
 const logoYouTube = document.querySelector("#logoYouTube");
 logoYouTube.addEventListener("mouseover", function() {
-    logoYouTube.src = "./assets/logos/YouTube_black.png";
+    logoYouTube.src = "./assets/logos/YouTube_black.webp";
 })
 logoYouTube.addEventListener("mouseout", function() {
-    logoYouTube.src = "./assets/logos/YouTube_red.png";
+    logoYouTube.src = "./assets/logos/YouTube_red.webp";
 })
 
 const logoSpotify = document.querySelector("#logoSpotify");
 logoSpotify.addEventListener("mouseover", function() {
-    logoSpotify.src = "./assets/logos/Spotify_black.png";
+    logoSpotify.src = "./assets/logos/Spotify_black.webp";
 })
 logoSpotify.addEventListener("mouseout", function() {
-    logoSpotify.src = "./assets/logos/Spotify_red.png";
+    logoSpotify.src = "./assets/logos/Spotify_red.webp";
+})
+
+
+//Apparition des icônes
+document.addEventListener('DOMContentLoaded', () => {
+    const iconesHeaderHome = document.querySelectorAll('.icones img');
+    
+    iconesHeaderHome[0].style.transform = 'scale(1)';
+    iconesHeaderHome[0].style.opacity = '1';
+    iconesHeaderHome[1].style.transform = 'scale(1)';
+    iconesHeaderHome[1].style.opacity = '1';
 })
